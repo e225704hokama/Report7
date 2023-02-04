befec572917bef20b4f69874563d803c32c6e7bf
Hokama Reo <e225704@ie.u-ryukyu.ac.jp>
2023-02-04 18:23:52 +0900

    3rd commit

--- Report7.c
+++ Report7.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 
 void sortScores(int scores[], int size);
+void printScores(int scores[], int size);
 
 void sortScores(int scores[], int size){
-    int number = 0;
-    int index ;
-    while(size - (number+1)){
-        int max = scores[number];
-        for(int i=0; i < size-1; i++){
-            if(max <= scores[i + number]){
+    int number = 0; //配列の場所を記憶しておく
+    int max; //最大値を記憶しておく
+    int index; //最大値の場所を記憶しておく
+
+    while((size - 1) - number){ //最後の要素はただ一つ決まるため大小判別は要らない
+        max = scores[number];
+        index  = number;
+
+        for(int i=0; i < (size - number); i++){ //下記の動作より、ループ回数を減らさなければいけない
+            if(max < scores[i + number]){ //numberをプラスすることで一周目に最大値を代入した配列の０を二周目には無視できる。以降の周回も同じ
                 max = scores[i + number];
                 index = i + number;
             }
+            if(scores[i] > 100){
+                printf("100点を超える要素が存在します。\n");
+                printf("配列の%d番目の値が %d >100です。\n", i+1, scores[i]);
+                number = size - 1;
+                break;
+            }
         }
-        scores[index] = scores[number];
+        scores[index] = scores[number]; //最大値はmaxに保管しているため上書きしても大丈夫
         scores[number] = max;
         number += 1;
     }
-    
 
+    printScores(scores, size);
     
+}
+
+void printScores(int scores[], int size){ //配列の要素を出力する関数
+    printf("scores = ");
+    for(int i=0; i < size; i++){
+        printf("%d ", scores[i]);
+    }
+    printf("\n");
 }
 
 int main(){
@@ -27,10 +46,11 @@ int main(){
     int scores[SIZE] = {0, 60, 70, 100, 90};
     //処理コード
     
-    printf("scores = %d %d %d %d %d\n", scores[0], scores[1], scores[2], scores[3], scores[4]);
+    printf("元の配列↓\n");
+    printScores(scores, SIZE);
 
+    printf("並び替えた配列↓\n");
     sortScores(scores, SIZE);
-    printf("results = %d %d %d %d %d\n", scores[0], scores[1], scores[2], scores[3], scores[4]);
-
+    
     return 0;
 }
